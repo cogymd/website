6b84074b24b22099c4d2b22c4d5735c414585c77
Spacepuffin <Spacepuffin7@gmail.com>
2019-07-16 11:13:45 -0400

    Scroll Settings

--- Script.js
+++ Script.js
@@ -1,4 +1,21 @@
+var safari;
+if(navigator.userAgent.indexOf("Safari") != -1)
+	{
+		safari = true;
+	}
+
+//Sidebar buttons
+scrSettings = {behavior:"smooth", block:"start", inline:"nearest"}
 function toTop(){
-	var top = document.getElementById("center");
-	top.scrollIntoView();
+	var top = document.getElementById("top");
+	if safari{
+		top.scrollIntoView();
+	}
+	else{
+		top.scrollIntoView(scrSettings);
+	}
+	
+	console.log("Scrolled to top")
 }
+
+//
