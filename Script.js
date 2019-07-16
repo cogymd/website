723996a6acd0419857dd75a197a2223d43392eb0
Spacepuffin <Spacepuffin7@gmail.com>
2019-07-16 13:16:25 -0400

    Collapse Sidebar

--- Script.js
+++ Script.js
@@ -18,3 +18,45 @@ function goTo(n){
 	}
 	console.log("Scrolled to", anchors[n])
 }
+
+//Toggle sidebar
+sidebarOn = true;
+sidebarPos = 0;
+var slide;
+function toggleSidebar(){
+	clearInterval(slide);
+	slide = setInterval(moveSidebar, 5);
+}
+
+function moveSidebar(){
+	sidebar = document.getElementById("sidebar");
+	if (sidebarOn){
+		if (sidebarPos <= -25){
+			clearInterval(slide);
+			sidebarOn = false;
+		}
+		else{
+			sidebarPos -= 0.5;
+			sidebar.style.left = sidebarPos + "%";
+		}
+	}
+	else{
+		if (sidebarPos >= 0){
+			clearInterval(slide);
+			sidebarOn = true;
+			sidebar.style.left = "0%";
+		}
+		else{
+			sidebarPos += 0.5;
+			sidebar.style.left = sidebarPos + "%";
+		}
+	}
+
+	sidebarButton = document.getElementById("toggleSidebar");
+	if (sidebarPos> -17){
+		sidebarButton.style.left = (sidebarPos+18) + "%";
+	}
+	else{
+		sidebarButton.style.left = "1%";
+	}
+}
